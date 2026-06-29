ec9f7826906027140f8567d8ffe1c1a8bd7825c0
priyanshu mittal <priyanshumittal0916@gmail.com>
2026-06-29 21:00:59 +0530

    Concept: add @EnableAsync

--- orchestrator/src/main/java/com/jobfit/orchestrator/OrchestratorApplication.java
+++ orchestrator/src/main/java/com/jobfit/orchestrator/OrchestratorApplication.java
@@ -2,8 +2,10 @@ package com.jobfit.orchestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class OrchestratorApplication {
 
     public static void main(String[] args) {
